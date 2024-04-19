7f3b9886d5f730c2e3a63ce98866b1c7b6e53214
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2024-04-19 15:16:00 -0500

    Add code for reading and extracting content

--- neural_docstrings/main.py
+++ neural_docstrings/main.py
@@ -2,8 +2,28 @@ from langchain_community.llms.ollama import Ollama
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.runnables.base import RunnableSequence
+from pathlib import Path
+from typing import List
 
-def main(systemPrompt: str, prompt: str)    ->  None:
+def readFile(path: Path)    ->  str:
+    with open(file=path, mode="r") as sourceFile:
+        code: List[str] = sourceFile.readlines()
+        sourceFile.close()
+
+    return "".join(code)
+
+
+def segmentFunctions(code: str) ->  List[str]:
+    return [c.strip() for c in code.split(sep="def ")[1:]]
+
+
+def main()    ->  None:
+    systemPrompt: str = "This file contains code for counting lines of code of software projects. Generate suitable docstring for these Python functions. Do not explain the result"
+
+    code: str = readFile(path=Path("./scc.py.test"))
+    functions: List[str] = segmentFunctions(code=code)
+
+
     output_parser = StrOutputParser()
     chatPrompt: ChatPromptTemplate = ChatPromptTemplate.from_messages([
         ("system", systemPrompt),
@@ -14,7 +34,7 @@ def main(systemPrompt: str, prompt: str)    ->  None:
     
     chain: RunnableSequence = chatPrompt | llm | output_parser
 
-    print(chain.invoke({"input": prompt}))
+    print(chain.invoke({"input": functions[1]}))
 
 
 if __name__ == "__main__":
