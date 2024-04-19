acbbc68c30a49e4f73b2db20f0418c30fde9df8f
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2024-04-19 15:48:40 -0500

    Add wrapper code to save docstrings to file

--- neural_docstrings/main.py
+++ neural_docstrings/main.py
@@ -1,11 +1,21 @@
-from langchain_community.llms.ollama import Ollama
-from langchain_core.prompts import ChatPromptTemplate
-from langchain_core.output_parsers import StrOutputParser
-from langchain_core.runnables.base import RunnableSequence
 from pathlib import Path
 from typing import List
 
-def readFile(path: Path)    ->  str:
+import click
+from langchain_community.llms.ollama import Ollama
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.runnables.base import RunnableSequence
+from progress.bar import Bar
+from pyfs import resolvePath
+
+SYSTEM_PROMPT: str = """This file contains code for counting lines of code of software projects.
+Generate suitable docstring for these Python functions in Google's style.
+Do not explain the result.
+Return as raw text"""
+
+
+def readFile(path: Path) -> str:
     with open(file=path, mode="r") as sourceFile:
         code: List[str] = sourceFile.readlines()
         sourceFile.close()
@@ -13,30 +23,68 @@ def readFile(path: Path)    ->  str:
     return "".join(code)
 
 
-def segmentFunctions(code: str) ->  List[str]:
+def segmentFunctions(code: str) -> List[str]:
     return [c.strip() for c in code.split(sep="def ")[1:]]
 
 
-def main()    ->  None:
-    systemPrompt: str = "This file contains code for counting lines of code of software projects. Generate suitable docstring for these Python functions. Do not explain the result"
-
-    code: str = readFile(path=Path("./scc.py.test"))
-    functions: List[str] = segmentFunctions(code=code)
-
-
+def inference(systemPrompt: str, code: str, model: str = "codegemma") -> str:
     output_parser = StrOutputParser()
-    chatPrompt: ChatPromptTemplate = ChatPromptTemplate.from_messages([
-        ("system", systemPrompt),
-        ("user", "{input}")
-    ])
+    chatPrompt: ChatPromptTemplate = ChatPromptTemplate.from_messages(
+        [("system", systemPrompt), ("user", "{input}")]
+    )
 
-    llm: Ollama = Ollama(model="llama2")
-    
+    llm: Ollama = Ollama(model=model)
+
     chain: RunnableSequence = chatPrompt | llm | output_parser
 
-    print(chain.invoke({"input": functions[1]}))
+    return chain.invoke({"input": code})
+
+
+@click.command()
+@click.option(
+    "-s",
+    "--system",
+    "systemPrompt",
+    type=str,
+    required=False,
+    default=SYSTEM_PROMPT,
+    help="System prompt",
+)
+@click.option(
+    "-i",
+    "--input",
+    "sourceFile",
+    type=Path,
+    required=True,
+    help="File to analyze",
+)
+@click.option(
+    "-o",
+    "--output",
+    "output",
+    type=Path,
+    required=True,
+    help="File to output to",
+)
+def main(systemPrompt: str, sourceFile: Path, output: Path) -> None:
+    data: List[str] = []
+    sf: Path = resolvePath(path=sourceFile)
+
+    code: str = readFile(path=sf)
+    functions: List[str] = segmentFunctions(code=code)
+
+    with Bar("Generating docstrings...", max=len(functions)) as bar:
+        func: str
+        for func in functions:
+            data.append(inference(systemPrompt=systemPrompt, code=func))
+            bar.next()
+
+    with open(file=output, mode="w") as outputFile:
+        code: str
+        for code in data:
+            outputFile.write(code + "\n\n---\n\n")
+        outputFile.close()
 
 
 if __name__ == "__main__":
     main()
-
